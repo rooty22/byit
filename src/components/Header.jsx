b3e5f04893d0d6294a31180980f4a8d7697ebe01
rooty22 <112939759+saeed169@users.noreply.github.com>
2023-11-24 00:01:31 +0200

    style respons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
 // Get Navbar
@@ -18,70 +18,72 @@ const GPlay = '/media/play.png'
 const Apply = '/media/apple.png'
 
 const Header = () => {
-  return (
-    <div className='Header'>
-        <div className="container">
-            <div className="head-one">
+    // const [mob] = useState(false)
 
-                <div className="logo">
-                    <img src={Logo} alt="Logo" />
-                </div>
-                <div className="menu">
-                    <MyNavbar />
-                </div>
-                <div className="app-download">
-                    <h5>حمل التطبيق الأن</h5>
-                    <div className="logos-down">
-                        <img src={GPlay} alt="Play" />
-                        <img src={Apply} alt="apple" style={{ maxWidth: '95px' }} />
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <div className="head-tow">
+    return (
+        <div className='Header'>
             <div className="container">
-                <div className="all-menu">
-                    <div className="menu-tow">
-                        <MyNavTow />
+                <div className="head-one">
+
+                    <div className="logo">
+                        <img src={Logo} alt="Logo" />
+                    </div>
+                    <div className="menu">
+                        <MyNavbar />
+                    </div>
+                    <div className="app-download">
+                        <h5>حمل التطبيق الأن</h5>
+                        <div className="logos-down">
+                            <img src={GPlay} alt="Play" />
+                            <img src={Apply} alt="apple" style={{ maxWidth: '95px' }} />
+                        </div>
                     </div>
 
-                    <div className="lango">
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                اللغة
-                            </Dropdown.Toggle>
+                </div>
+            </div>
+            <div className="head-tow">
+                <div className="container">
+                    <div className="all-menu">
+                        <div className="menu-tow">
+                            <MyNavTow />
+                        </div>
 
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">عربي</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">English</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
+                        <div className="lango">
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    اللغة
+                                </Dropdown.Toggle>
 
-                    <div className="social-ico">
-                        <Link to="https://google.com" className='whats'>
-                            <img src={Whatsapp} alt="Whats" />
-                        </Link>
-                        <Link to="https://google.com" className='face'>
-                            <img src={Insta} alt="Whats" />
-                        </Link>
-                        <Link to="https://google.com" className='linke'>
-                            <img src={Twitt} alt="Whats" />
-                        </Link>
-                        <Link to="https://google.com" className='twitt'>
-                            <img src={Snap} alt="Whats" />
-                        </Link>
-                        <Link to="https://google.com" className='twitt'>
-                            <img src={TicT} alt="Whats" />
-                        </Link>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">عربي</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">English</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+
+                        <div className="social-ico">
+                            <Link to="https://google.com" className='whats'>
+                                <img src={Whatsapp} alt="Whats" />
+                            </Link>
+                            <Link to="https://google.com" className='face'>
+                                <img src={Insta} alt="Whats" />
+                            </Link>
+                            <Link to="https://google.com" className='linke'>
+                                <img src={Twitt} alt="Whats" />
+                            </Link>
+                            <Link to="https://google.com" className='twitt'>
+                                <img src={Snap} alt="Whats" />
+                            </Link>
+                            <Link to="https://google.com" className='twitt'>
+                                <img src={TicT} alt="Whats" />
+                            </Link>
+                        </div>
+                        
                     </div>
-                    
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
